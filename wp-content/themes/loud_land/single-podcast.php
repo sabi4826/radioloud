941c3fb8563df77b0337ca74aec8046147d1bed2
pern7295 <pern7295@stud.kea.dk>
2021-04-21 17:29:33 +0200

    forsøg på at få episoder vist i single-view

--- wp-content/themes/loud_land/single-podcast.php
+++ wp-content/themes/loud_land/single-podcast.php
@@ -25,6 +25,21 @@ get_header(); ?>
                 </div>
             </article>
 
+            <section id="podcast_oversigt1">
+                <h1>Episoder</h1>
+            </section>
+
+            <!--NYT!!-->
+            <template class="template1">
+                <article>
+                    <img src="" alt="" class="billede">
+                    <h2></h2>
+                    <p class="dato"></p>
+                    <p class="episode_resume"></p>
+                    <p class="vaerter"></p>
+                </article>
+            </template>
+
 
         </main>
         <!-- #main -->
@@ -53,7 +68,60 @@ get_header(); ?>
 
             getJson();
 
+            //NYT!!!!!
+
+            let podcasts1;
+            let categories1;
+
+            // container/destination til articles med en podcast
+            const dest = document.querySelector("#podcast_oversigt1");
+            // select indhold af html skabelon (article)
+            const skabelon = document.querySelector("template1");
+            // url til wp rest api/database
+            const url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/podcast?per_page=100";
+            const cat_url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/categories";
+
+            async function loadJson() {
+                const JsonData = await fetch(url);
+                const catData = await fetch(cat_url);
+                podcasts1 = await JsonData.json();
+                categories1 = await catData.json();
+                console.log("loadJson");
+                visPodcasts1();
+            }
+
+            //funktion, der viser podcasts i liste view
+            function visPodcasts1() {
+                console.log("visPodcasts-funktion");
+                // ryd ekst. indhold:
+                dest.innerHTML = "";
+
+                // loop igennem json (lande)
+                podcasts1.forEach(podcast => {
+
+                    //if (filter == podcast.kategori || filter == "alle")
+
+                    const klon = skabelon.cloneNode(true).content;
+                    klon.querySelector(".billede").src = podcast.billede.guid;
+                    klon.querySelector("h2").textContent = podcast.title.rendered;
+                    klon.querySelector(".episode_resume").textContent = podcast.episode_resume;
+                    klon.querySelector(".dato").textContent = podcast.dato;
+                    klon.querySelector(".vaerter").textContent = `${"Værter: "}` + podcast.vaerter;
+
+                    // eventlisteners på hver enkelt artikel
+                    klon.querySelector("article").addEventListener("click", () => {
+                        location.href = podcast.link;
+                    })
+
+                    dest.appendChild(klon);
+
+                })
+            }
+
+            loadJson();
+
         </script>
+
 
     </div>
     <!-- #primary -->
