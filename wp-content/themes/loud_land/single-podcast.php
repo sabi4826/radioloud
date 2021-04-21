04318d707de5ebdb2dc2e1bcd7f91b2a0cfb76cc
pern7295 <pern7295@stud.kea.dk>
2021-04-21 18:13:04 +0200

    single-view episoder

--- wp-content/themes/loud_land/single-podcast.php
+++ wp-content/themes/loud_land/single-podcast.php
@@ -25,8 +25,22 @@ get_header(); ?>
                 </div>
             </article>
 
+            <section>
+                <h1>Episoder</h1>
+                <div class="episode-section"></div>
+            </section>
+
         </main>
         <!-- #main -->
+
+        <template id="episode-afsnit">
+            <article>
+                <img src="" alt="" class="episode-billede">
+                <h2></h2>
+                <h4></h4>
+                <p></p>
+            </article>
+        </template>
 
         <script>
             let podcast;
@@ -34,11 +48,20 @@ get_header(); ?>
             //Konstanten sættes til at lede efter "podcast" - herefter laves et echo-kald, hvor der beder om den pågældende podcasts' ID, alt efter hvilken der klikkes på
             const dbUrl = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/podcast/" + <?php echo get_the_ID() ?>;
 
+            //EPISODE
+            const epiUrl = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/episode?per_page=100";
+
 
             async function getJson() {
                 const data = await fetch(dbUrl);
                 podcast = await data.json();
                 visPodcasts();
+
+
+                //EPISODE
+                const epiData = await fetch(epiUrl);
+                episode = await epiData.json();
+                visEpisoder();
             }
 
 
@@ -47,6 +70,30 @@ get_header(); ?>
                 document.querySelector("h2").textContent = podcast.title.rendered;
                 document.querySelector(".podcast_resume").textContent = podcast.podcast_resume;
                 document.querySelector(".vaerter").textContent = `${"Værter: "}` + podcast.vaerter;
+
+            }
+
+            function visEpisoder() {
+                console.log("visEpisoder-funktion");
+
+                // loop igennem json (epsidoer)
+                episoder.forEach(episode => {
+
+                    const skabelon = document.querySelector("template");
+                    const dest = document.querySelector("#episode-afsnit");
+
+                    const klon = skabelon.cloneNode(true).content;
+                    klon.querySelector(".billede").src = episode.billede.guid;
+                    klon.querySelector("h2").textContent = episode.title.rendered;
+                    klon.querySelector("h4").textContent = episode.dato;
+                    klon.querySelector("p").textContent = episode.episode_resume;
+
+
+                    dest.appendChild(klon);
+
+                })
+
+
 
             }
 
