9585bff31fc34479faddb446b7f6fe0974c6e7e6
Ida-23 <idax7887@stud.kea.dk>
2021-04-21 13:16:56 +0200

    forsiden har fået hentet data ind for første gang

--- wp-content/themes/loud_land/page-index.php
+++ wp-content/themes/loud_land/page-index.php
@@ -15,19 +15,86 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<div class="singular-content-wrap">
-			<?php
-				while ( have_posts() ) : the_post();
+    <main id="main" class="site-main">
+        <div class="singular-content-wrap"></div> <!-- .singular-content-wrap -->
+        <section>
+            <h1 id="overskrift">Nye podcast episoder fra LOUD</h1>
+            <div class="new_podcast"></div>
+        </section>
 
-					get_template_part( 'template-parts/content/content', 'page' );
 
-					get_template_part( 'template-parts/content/content', 'comment' );
+    </main><!-- #main -->
 
-				endwhile; // End of the loop.
-				?>
-		</div> <!-- .singular-content-wrap -->
-	</main><!-- #main -->
+    <template>
+        <article>
+            <img src="" alt="" class="billede">
+            <h2></h2>
+            <h4></h4>
+            <p class="podcast_resume"></p>
+            <button class="red_knap">Gå til podcast</button>
+        </article>
+    </template>
+
+
+
+    <script>
+        let podcasts;
+
+        // url til wp rest api/database
+        const url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/podcast?per_page=100";
+
+
+        async function loadJson() {
+            const JsonData = await fetch(url);
+            newEoisoder = await JsonData.json();
+
+            console.log("loadJson");
+            visNewPodcast();
+
+        }
+
+
+
+        //Her i funktioen genereres tre til fire tilfeldeig andre kunstværker og sættes ind i HTML
+        function visNewPodcast() {
+            console.log("visNewPodcast");
+
+            //Genererer et nyt array af tilfældige objekter fra det komplette array
+            const other1 = newEoisoder[Math.floor(Math.random() * newEoisoder.length)];
+            const other2 = newEoisoder[Math.floor(Math.random() * newEoisoder.length)];
+            const other3 = newEoisoder[Math.floor(Math.random() * newEoisoder.length)];
+            const randomArt = [other1, other2, other3];
+            console.log(randomEpisode);
+
+            randomEpisode.forEach(podcasts => {
+                //Definerer konstanter til senere brug i kloningen af template
+                const template = document.querySelector("template");
+                const container = document.querySelector(".new_podcast");
+
+
+                const klon = skabelon.cloneNode(true).content; //Her klones template og udfyldes med data fra de tilfældige objekter
+                klon.querySelector(".billede").src = podcast.billede.guid;
+                klon.querySelector("h2").textContent = podcast.title.rendered;
+                klon.querySelector("h4").textContent = podcast.title.rendered;
+                klon.querySelector(".podcast_resume").textContent = podcast.podcast_resume;
+
+
+                // eventlisteners på hver enkelt artikel
+                klon.querySelector("article").addEventListener("click", () => {
+                    location.href = podcast.link;
+                })
+
+                dest.appendChild(klon);
+            })
+
+        }
+
+    </script>
+
+
+
+
+
 </div><!-- #primary -->
 
 <?php get_sidebar();
