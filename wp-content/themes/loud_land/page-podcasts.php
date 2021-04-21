26a6dc7ecf8aef43b98ad3f82df1170e95e4f852
Ida-23 <idax7887@stud.kea.dk>
2021-04-21 12:14:49 +0200

    har ikke ændret noget

--- wp-content/themes/loud_land/page-podcasts.php
+++ wp-content/themes/loud_land/page-podcasts.php
@@ -15,97 +15,97 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
-	<main id="main" class="site-main">
-		<h1 id="overskrift">Podcasts</h1>
-		<nav id="filtrering">
-			<button data-podcast="alle">Alle</button>
-		</nav>
-		<section id="podcast_oversigt"></section>
-	</main><!-- #main -->
+    <main id="main" class="site-main">
+        <h1 id="overskrift">Podcasts</h1>
+        <nav id="filtrering">
+            <button data-podcast="alle">Alle</button>
+        </nav>
+        <section id="podcast_oversigt"></section>
+    </main><!-- #main -->
 
-	<template>
-		<article>
-			<img src="" alt="" class="billede">
-			<h2></h2>
-			<p class="podcast_resume"></p>
-			<p class="vaerter"></p>
-		</article>
-	</template>
+    <template>
+        <article>
+            <img src="" alt="" class="billede">
+            <h2></h2>
+            <p class="podcast_resume"></p>
+            <p class="vaerter"></p>
+        </article>
+    </template>
 
-	<script>
-		let podcasts;
-		let categories;
-		let filterPodcast = "alle";
-		// container/destination til articles med en podcast
-		const dest = document.querySelector("#podcast_oversigt");
-		// select indhold af html skabelon (article)
-		const skabelon = document.querySelector("template");
-		// url til wp rest api/database
-		const url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/podcast?per_page=100";
-		const cat_url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/categories";
+    <script>
+        let podcasts;
+        let categories;
+        let filterPodcast = "alle";
+        // container/destination til articles med en podcast
+        const dest = document.querySelector("#podcast_oversigt");
+        // select indhold af html skabelon (article)
+        const skabelon = document.querySelector("template");
+        // url til wp rest api/database
+        const url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/podcast?per_page=100";
+        const cat_url = "http://sabineovesen.dk/radioloud/wp-json/wp/v2/categories";
 
-		async function loadJson() {
-			const JsonData = await fetch(url);
-			const catData = await fetch(cat_url);
-			podcasts = await JsonData.json();
-			categories = await catData.json();
-			console.log("loadJson");
-			visPodcasts();
-			opretKnapper();
-		}
+        async function loadJson() {
+            const JsonData = await fetch(url);
+            const catData = await fetch(cat_url);
+            podcasts = await JsonData.json();
+            categories = await catData.json();
+            console.log("loadJson");
+            visPodcasts();
+            opretKnapper();
+        }
 
-		function opretKnapper() {
-			console.log("opretKnapper virker");
-			categories.forEach(cat => {
-				document.querySelector("#filtrering").innerHTML += `<button class="filter" data-podcast="${cat.id}">${cat.name}</button>`
-			})
-			addEventListenersToButtons();
-		}
+        function opretKnapper() {
+            console.log("opretKnapper virker");
+            categories.forEach(cat => {
+                document.querySelector("#filtrering").innerHTML += `<button class="filter" data-podcast="${cat.id}">${cat.name}</button>`
+            })
+            addEventListenersToButtons();
+        }
 
-		function addEventListenersToButtons() {
-			console.log("lytTilKnapper virker");
-			document.querySelectorAll("#filtrering button").forEach(elm => {
-				elm.addEventListener("click", filtrering);
-			})
-		};
+        function addEventListenersToButtons() {
+            console.log("lytTilKnapper virker");
+            document.querySelectorAll("#filtrering button").forEach(elm => {
+                elm.addEventListener("click", filtrering);
+            })
+        };
 
-		function filtrering() {
-			filterPodcast = this.dataset.podcast;
-			console.log("filterPodcast");
-			visPodcasts();
-		}
+        function filtrering() {
+            filterPodcast = this.dataset.podcast;
+            console.log("filterPodcast");
+            visPodcasts();
+        }
 
-		//funktion, der viser podcasts i liste view
-		function visPodcasts() {
-			console.log("visPodcasts-funktion");
-			// ryd ekst. indhold:
-			dest.innerHTML = "";
+        //funktion, der viser podcasts i liste view
+        function visPodcasts() {
+            console.log("visPodcasts-funktion");
+            // ryd ekst. indhold:
+            dest.innerHTML = "";
 
-			// loop igennem json (lande)
-			podcasts.forEach(podcast => {
+            // loop igennem json (lande)
+            podcasts.forEach(podcast => {
 
-				//if (filter == podcast.kategori || filter == "alle")
-				if (filterPodcast == "alle" || podcast.categories.includes(parseInt(filterPodcast))) {
+                //if (filter == podcast.kategori || filter == "alle")
+                if (filterPodcast == "alle" || podcast.categories.includes(parseInt(filterPodcast))) {
 
-					const klon = skabelon.cloneNode(true).content;
-					klon.querySelector(".billede").src = podcast.billede.guid;
-					klon.querySelector("h2").textContent = podcast.title.rendered;
-					klon.querySelector(".podcast_resume").textContent = podcast.podcast_resume;
-					klon.querySelector(".vaerter").textContent = `${"Værter: "}` + podcast.vaerter;
+                    const klon = skabelon.cloneNode(true).content;
+                    klon.querySelector(".billede").src = podcast.billede.guid;
+                    klon.querySelector("h2").textContent = podcast.title.rendered;
+                    klon.querySelector(".podcast_resume").textContent = podcast.podcast_resume;
+                    klon.querySelector(".vaerter").textContent = `${"Værter: "}` + podcast.vaerter;
 
-					// eventlisteners på hver enkelt artikel
-					klon.querySelector("article").addEventListener("click", () => {
-						location.href = podcast.link;
-					})
+                    // eventlisteners på hver enkelt artikel
+                    klon.querySelector("article").addEventListener("click", () => {
+                        location.href = podcast.link;
+                    })
 
-					dest.appendChild(klon);
-				}
-			})
-		}
+                    dest.appendChild(klon);
+                }
+            })
+        }
 
-		loadJson();
+        loadJson();
 
-	</script>
+    </script>
 
 </div><!-- #primary -->
 
