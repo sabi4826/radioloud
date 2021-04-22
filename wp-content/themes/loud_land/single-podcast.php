28fb251d525bd1d385e6e1cb005b141f17bc9a33
pern7295 <pern7295@stud.kea.dk>
2021-04-22 14:42:48 +0200

    e

--- wp-content/themes/loud_land/single-podcast.php
+++ wp-content/themes/loud_land/single-podcast.php
@@ -98,7 +98,7 @@ get_header(); ?>
 
                     console.log("horer_til_podcast bliver kaldt", episode.horer_til_podcast);
 
-                    if (episoder.horer_til_podcast[0].ID == aktuelPodcast);
+                    if (episoder.horer_til_podcast == aktuelPodcast);
                     console.log("Loop kører ID:", aktuelPodcast);
 
                     let klon = episodeTemplate.cloneNode(true).content;
