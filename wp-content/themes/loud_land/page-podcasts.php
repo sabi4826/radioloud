ad0ab78bc6f272204c0157764f28bb5a41483905
Ida-23 <idax7887@stud.kea.dk>
2021-04-27 14:14:54 +0200

    baggrundsbillede

--- wp-content/themes/loud_land/page-podcasts.php
+++ wp-content/themes/loud_land/page-podcasts.php
@@ -21,7 +21,7 @@ get_header(); ?>
         background-image: url(http://sabineovesen.dk/radioloud/wp-content/uploads/2021/04/Podcasts_splash.png);
         background-size: cover;
         max-width: none;
-        height: 30vh;
+        height: 50vh;
         width: auto;
         margin: 0px -100px 0px -100px;
     }
@@ -74,8 +74,10 @@ get_header(); ?>
     </header>
 
     <main id="main" class="site-main">
-        <div class="podcast_splashbillede"></div>
-        <h1 id="overskrift">Podcasts</h1>
+        <div class="podcast_splashbillede">
+            <h1 id="overskrift">Podcasts</h1>
+        </div>
+
         <nav id="filtrering">
             <button class="filter_knapper" data-podcast="alle">Alle</button>
         </nav>
